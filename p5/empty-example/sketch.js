27fbbb5ea57f8cf5410e90c6856bfdb62bd6a3de
kriso <kristian.knack@griffithuni.edu.au>
2018-12-16 16:36:39 +1000

    initialising the repository with foundation files

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  // put setup code here
+    createCanvas(550, 750);
+}
+
+function draw() {
+  // put drawing code here
+    background(100)
+    rectMode(CENTER)
+    fill('yellow')
+    rect( 275, 10, 5, 20);
+    rect( 275, 60, 5, 20);
+    rect( 275, 110, 5, 20);
+    rect( 275, 160, 5, 20);
+    rect( 275, 210, 5, 20);
+    rect( 275, 260, 5, 20);
+    rect( 275, 310, 5, 20);
+    rect( 275, 360, 5, 20);
+    rect( 275, 410, 5, 20);
+    rect( 275, 460, 5, 20);
+    rect( 275, 510, 5, 20);
+    rect( 275, 560, 5, 20);
+    rect( 275, 610, 5, 20);
+    rect( 275, 660, 5, 20);
+    rect( 275, 710, 5, 20);
+    rect( 275, 760, 5, 20);
+    fill('blue');
+    rect(200,700, 30, 50);
+    fill('red')
+    ellipse(100, 200, 30);
+    ellipse(400, 400, 30);
+    
+}
